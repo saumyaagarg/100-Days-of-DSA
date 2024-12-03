9f733dbab704b378c21f175562e244aac998ef10
Saumyaa Garg <129076462+saumyaagarg@users.noreply.github.com>
2024-12-03 16:07:53 +0530

    Add files via upload

---
+++ Day 04/1-reverse-string.cpp
@@ -0,0 +1,26 @@
+// Write a program to reverse a string.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Function to reverse a string
+void reverse_str(string str) {
+    int n = str.length();
+    string rev_str = str;     // Initialize rev_str with the same size as str
+
+    // Reverse the string
+    for (int j = n - 1, i = 0; j >= 0; j--, i++) {
+        rev_str[i] = str[j];
+    }
+
+    cout << "The reversed string is: " << rev_str << endl;
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);       // Use getline to read the full line including spaces
+    reverse_str(str);
+    return 0;
+}
---
+++ Day 04/4-remove-duplicates.cpp
@@ -0,0 +1,41 @@
+// Implement a program to remove duplicate characters from a string.
+
+#include <iostream>
+using namespace std;
+
+// Function to check if a particular character is present in a string
+bool is_present(char ch, string str) {
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == ch) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Function to remove duplicate characters from a string
+void remove_duplicates(string str) {
+    int n = str.size();
+    int newLength = 0;
+
+    // Iterate through the string
+    for (int i = 0; i < n; i++) {
+        // If the character is not already in the result, move it to the 'new' part of the string
+        if (!is_present(str[i], str.substr(0, newLength))) {
+            str[newLength] = str[i];
+            newLength++;
+        }
+    }
+
+    // Cut off any extra characters after the new length
+    str = str.substr(0, newLength);
+
+    cout << "String after removing duplicate characters is: " << str;
+}
+
+int main() {
+    string str;
+    cout << "Enter the string: ";
+    cin >> str;
+    remove_duplicates(str);
+}
